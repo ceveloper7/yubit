025582a813ecd9ecfc6e79cd3ec65c163f93f33c
ceveloper7 <cevabarcvilla@gmail.com>
2026-05-06 03:53:51 +0000

    Yubit eCommerce

---
+++ src/main/java/com/gba/yubit/YubitApplication.java
@@ -0,0 +1,13 @@
+package com.gba.yubit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YubitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YubitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gba/yubit/YubitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gba.yubit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YubitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
